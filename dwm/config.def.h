7f71f2ae51382ebb5b24b6edd73d5f931b6dcd4e
apePetrus <shiveringveteran@proton.me>
2025-06-24 17:56:08 -0300

    FEAT: changed dwm font

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,8 +7,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[] = {
+    "CaskaydiaCove Nerd Font Mono:style:medium:size=12",
+    "CaskaydiaCove Nerd Font Mono:style:medium:size=19"};
+static const char dmenufont[] = "CaskaydiaCove Nerd Font Mono:pixelsize=15";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
