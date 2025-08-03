31e2f2707f9356a434c2941abfcec502154e722a
Pedro Pereira <pedro.hr.pereira@proton.me>
2025-08-02 22:43:49 -0300

    FEAT: Icons to dwmblocks to better understanding
    Commented the battery line, since I am sharing this between desktop and laptop

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                            /*Update Interval*/	    /*Update Signal*/
-	{ "",        "date '+%d/%b/%Y (%a) %I:%M%p'"                           , 60,                     10 },
-    { "",        "amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }'", 0 ,                     10 },
-    { "",        "acpi | awk '{print $4}' | sed s/,//"                     , 10,                     21 },
+	{ "",        "date '+󰃭 %d/%b (%a)  %I:%M%p'"                           , 60,                     10 },
+    { "",        "echo \"󰕾 $(amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }')\"", 0 ,                     10 },
+    // { "",        "acpi | awk '{print $4}' | sed s/,//"                     , 10,                     21 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
