90514c12d0961c9e2de7661e2bc63b38a58a43dd
Pedro Pereira <pedro.hr.pereira@proton.me>
2025-08-05 23:06:03 -0300

    FEAT: Icon for battery

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                            /*Update Interval*/	    /*Update Signal*/
 	{ "",        "date '+󰃭 %d/%b (%a)  %I:%M%p'"                           , 60,                     10 },
     { "",        "echo \"󰕾 $(amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }')\"", 0 ,                     10 },
-    // { "",        "acpi | awk '{print $4}' | sed s/,//"                     , 10,                     21 },
+    // { "",        "echo \" $(acpi | awk '{print $4}' | sed s/,//)\""                     , 10,                     21 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
