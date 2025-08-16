bf2595134908f322b09222d425a309f39d768765
Pedro Pereira <pedro.hr.pereira@proton.me>
2025-08-16 12:21:00 -0300

    FEAT: changed modkey to Alt

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -74,7 +74,7 @@ static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
