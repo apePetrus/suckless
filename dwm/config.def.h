8f6cad13376ea95e41a98eb5005dceccc222403e
Pedro Pereira <pedro.hr.pereira@proton.me>
2026-05-12 00:32:16 -0300

    dwm - new config overwrites old config

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -4,32 +4,24 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[] = {
-    "CaskaydiaCove Nerd Font Mono:style:medium:size=12",
-    "CaskaydiaCove Nerd Font Mono:style:medium:size=19"};
-static const char dmenufont[] = "CaskaydiaCove Nerd Font Mono:pixelsize=15";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#313244";
-static char selbordercolor[]        = "#cba6f7";
-static char selbgcolor[]            = "#cba6f7";
-static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+static const char *fonts[]    = {"CaskaydiaCove Nerd Font Mono:style:medium:size=12"};
+static const char dmenufont[] =  "CaskaydiaCove Nerd Font Mono:pixelsize=15";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-static char *tags[] = {"一", "二", "三", "四", "五"};
+static const char *tags[] = {"一", "二", "三", "四", "五"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,34 +36,18 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55;       /* factor of master area size [0.05..0.95] */
-static const int   nmaster = 1;        /* number of clients in master area */
-static const int   resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int   lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#include "vanitygaps.c"
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
-	{ "###",      nrowgrid },
-	{ "---",      horizgrid },
-	{ ":::",      gaplessgrid },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ NULL,       NULL },
+	{ "[M]",      monocle },
 };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 /* key definitions */
 #define MODKEY Mod4Mask
@@ -81,80 +57,84 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
+#define LIGHTUP   XF86XK_MonBrightnessUp
+#define LIGHTDOWN XF86XK_MonBrightnessDown
+
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "st", NULL };
 static const char *browsercmd[] = { "librewolf", NULL };
 
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+
 static const Key keys[] = {
-	/* modifier	                 key                        function        argument */
+	/* modifier                     key        function        argument */
 
-	{ MODKEY,	                   XK_p,                      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,	         XK_Return,                 spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             XK_l,      spawn,          SHCMD("slock") },
 
-	/* Brightness and audio */
-	{ MODKEY|ShiftMask,          XK_equal,                  spawn,          SHCMD("amixer -q sset Master 5%+ ; pkill -RTMIN+10 dwmblocks")},
-	{ MODKEY|ShiftMask,          XK_minus,                  spawn,          SHCMD("amixer -q sset Master 5%- ; pkill -RTMIN+10 dwmblocks")},
-	{ MODKEY|ShiftMask,          XK_m,                      spawn,          SHCMD("amixer -q sset Master toggle ; pkill -RTMIN+10 dwmblocks")},
-	{ 0,                         XF86XK_MonBrightnessUp,    spawn,          SHCMD("brightnessctl s 5+ ; pkill -RTMIN+10 dwmblocks")},
-	{ 0,                         XF86XK_MonBrightnessDown,  spawn,          SHCMD("brightnessctl s 5- ; pkill -RTMIN+10 dwmblocks")},
+  /* Brightness and audio */
+  // TODO Gotta fix these audio cmd later
+	{ MODKEY|ShiftMask,             XK_equal,  spawn,          SHCMD("amixer -q sset Master 5%+    ; pkill -RTMIN+10 dwmblocks")},
+	{ MODKEY|ShiftMask,             XK_minus,  spawn,          SHCMD("amixer -q sset Master 5%-    ; pkill -RTMIN+10 dwmblocks")},
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("amixer -q sset Master toggle ; pkill -RTMIN+10 dwmblocks")},
+	{ 0,                            LIGHTUP,   spawn,          SHCMD("brightnessctl s 5+")},
+	{ 0,                            LIGHTDOWN, spawn,          SHCMD("brightnessctl s 5-")},
 
-	/* Common software */
-	{ MODKEY,                    XK_w,                      spawn,          {.v = browsercmd}},
-	{ MODKEY|ShiftMask,          XK_w,                      spawn,          SHCMD("chromium")},
-	{ MODKEY,                    XK_e,                      spawn,          SHCMD("st -e yazi")},
-	{ MODKEY|ShiftMask,          XK_s,                      spawn,          SHCMD("flameshot gui")},
-	{ MODKEY|ShiftMask,          XK_g,                      spawn,          SHCMD("steam")},
-	{ MODKEY,                    XK_minus,                  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY|ShiftMask,          XK_d,                      spawn,          SHCMD("Discord")},
-	{ MODKEY|ShiftMask,          XK_f,                      spawn,          SHCMD("st -e newsboat")},
-	{ MODKEY|ShiftMask,          XK_t,                      spawn,          SHCMD("qbittorrent")},
-	{ MODKEY|ShiftMask,          XK_k,                      spawn,          SHCMD("keepassxc")},
+  /* Common software */
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd}},
+	{ MODKEY,                       XK_minus,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          SHCMD("st -e newsboat")},
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("flameshot gui")},
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          SHCMD("qbittorrent")},
+	{ MODKEY|ShiftMask,             XK_k,      spawn,          SHCMD("keepassxc")},
 
-	/* Quit, reboot, shutdown */
-	{ MODKEY|ShiftMask,          XK_e,                      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to quit?' 'killall dwm'")},
-	{ MODKEY|ShiftMask,          XK_x,                      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to shutdown the system?' " "'loginctl poweroff'")},
-	{ MODKEY|ShiftMask,          XK_r,                      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to reboot the system?' " "'loginctl reboot'")},
+  /* Quit, reboot, shutdown */
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to quit?' 'killall dwm'")},
+	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to shutdown the system?' " "'loginctl poweroff'")},
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          SHCMD("~/.local/bin/prompt 'Do you really want to reboot the system?' " "'loginctl reboot'")},
 
-	/* scripts */
-	{ MODKEY,                    XK_ccedilla,               spawn,          SHCMD("~/.local/bin/dmenuunicode")},
-    { MODKEY,                  XK_c,                      spawn,          SHCMD("caso-a-ana-quebre-meu-computador.sh")},
+  /* Scripts */
+	{ MODKEY,                       XK_ccedilla, spawn,        SHCMD("~/.local/bin/dmenuunicode")},
 
-	{ MODKEY,	                   XK_b,                      togglebar,      {0} },
-	{ MODKEY,	                   XK_j,                      focusstack,     {.i = +1 } },
-	{ MODKEY,	                   XK_k,                      focusstack,     {.i = -1 } },
-	{ MODKEY,	                   XK_i,                      incnmaster,     {.i = +1 } },
-	{ MODKEY,	                   XK_d,                      incnmaster,     {.i = -1 } },
-	{ MODKEY,	                   XK_h,                      setmfact,       {.f = -0.05} },
-	{ MODKEY,	                   XK_l,                      setmfact,       {.f = +0.05} },
-	{ MODKEY,	                   XK_Return,                 zoom,           {0} },
-	{ MODKEY,	                   XK_Tab,                    view,           {0} },
+  /* Navigation */
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
 
-	{ MODKEY|ShiftMask,	         XK_q,                      killclient,     {0} },
-	{ MODKEY,	                   XK_f,                      togglefullscr,  {0} },
-	{ MODKEY,	                   XK_t,                      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,	                   XK_m,                      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,	                   XK_space,                  setlayout,      {0} },
-	{ MODKEY|ShiftMask,	         XK_space,                  togglefloating, {0} },
-	{ MODKEY,	                   XK_0,                      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,	         XK_0,                      tag,            {.ui = ~0 } },
+  /* Layout */
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 
-	/* Navigation between multi monitor setup */
-	{ MODKEY,	                   XK_comma,                  focusmon,       {.i = -1 } },
-	{ MODKEY,	                   XK_period,                 focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,	         XK_comma,                  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,	         XK_period,                 tagmon,         {.i = +1 } },
+  /* Multi monitor navigation */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 
-	TAGKEYS(	                   XK_1,                      0)
-	TAGKEYS(	                   XK_2,                      1)
-	TAGKEYS(	                   XK_3,                      2)
-	TAGKEYS(	                   XK_4,                      3)
-	TAGKEYS(	                   XK_5,                      4)
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
 };
 
 /* button definitions */
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -35,7 +35,6 @@
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
-#include <X11/Xresource.h>
 #include <X11/Xutil.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
@@ -62,21 +61,6 @@
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
-#define XRDB_LOAD_COLOR(R,V)    if (XrmGetResource(xrdb, R, NULL, &type, &value) == True) { \
-                                  if (value.addr != NULL && strnlen(value.addr, 8) == 7 && value.addr[0] == '#') { \
-                                    int i = 1; \
-                                    for (; i <= 6; i++) { \
-                                      if (value.addr[i] < 48) break; \
-                                      if (value.addr[i] > 57 && value.addr[i] < 65) break; \
-                                      if (value.addr[i] > 70 && value.addr[i] < 97) break; \
-                                      if (value.addr[i] > 102) break; \
-                                    } \
-                                    if (i == 7) { \
-                                      strncpy(V, value.addr, 7); \
-                                      V[7] = '\0'; \
-                                    } \
-                                  } \
-                                }
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
@@ -108,7 +92,6 @@ typedef struct Client Client;
 struct Client {
 	char name[256];
 	float mina, maxa;
-	float cfact;
 	int x, y, w, h;
 	int oldx, oldy, oldw, oldh;
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
@@ -143,10 +126,6 @@ struct Monitor {
 	int by;               /* bar geometry */
 	int mx, my, mw, mh;   /* screen size */
 	int wx, wy, ww, wh;   /* window area  */
-	int gappih;           /* horizontal gap between windows */
-	int gappiv;           /* vertical gap between windows */
-	int gappoh;           /* horizontal outer gaps */
-	int gappov;           /* vertical outer gaps */
 	unsigned int seltags;
 	unsigned int sellt;
 	unsigned int tagset[2];
@@ -208,7 +187,6 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
-static void loadxrdb(void);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
@@ -218,7 +196,6 @@ static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
-static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
@@ -232,7 +209,6 @@ static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
 static void setlayout(const Arg *arg);
-static void setcfact(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
 static void seturgent(Client *c, int urg);
@@ -240,6 +216,7 @@ static void showhide(Client *c);
 static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
+static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
@@ -265,7 +242,6 @@ static Monitor *wintomon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
-static void xrdb(const Arg *arg);
 static void zoom(const Arg *arg);
 
 static pid_t getparentprocess(pid_t p);
@@ -534,7 +510,7 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + TEXTW(selmon->ltsymbol))
 			click = ClkLtSymbol;
-		else if (ev->x > selmon->ww - (int)TEXTW(stext))
+		else if (ev->x > selmon->ww - (int)TEXTW(stext) + lrpad - 2)
 			click = ClkStatusText;
 		else
 			click = ClkWinTitle;
@@ -580,7 +556,7 @@ cleanup(void)
 	for (i = 0; i < CurLast; i++)
 		drw_cur_free(drw, cursor[i]);
 	for (i = 0; i < LENGTH(colors); i++)
-		free(scheme[i]);
+		drw_scm_free(drw, scheme[i], 3);
 	free(scheme);
 	XDestroyWindow(dpy, wmcheckwin);
 	drw_free(drw);
@@ -734,10 +710,6 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
-	m->gappih = gappih;
-	m->gappiv = gappiv;
-	m->gappoh = gappoh;
-	m->gappov = gappov;
 	m->lt[0] = &layouts[0];
 	m->lt[1] = &layouts[1 % LENGTH(layouts)];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
@@ -964,14 +936,15 @@ focusstack(const Arg *arg)
 Atom
 getatomprop(Client *c, Atom prop)
 {
-	int di;
-	unsigned long dl;
+	int format;
+	unsigned long nitems, dl;
 	unsigned char *p = NULL;
 	Atom da, atom = None;
 
 	if (XGetWindowProperty(dpy, c->win, prop, 0L, sizeof atom, False, XA_ATOM,
-		&da, &di, &dl, &dl, &p) == Success && p) {
-		atom = *(Atom *)p;
+		&da, &format, &nitems, &dl, &p) == Success && p) {
+		if (nitems > 0 && format == 32)
+			atom = *(long *)p;
 		XFree(p);
 	}
 	return atom;
@@ -997,10 +970,10 @@ getstate(Window w)
 	Atom real;
 
 	if (XGetWindowProperty(dpy, w, wmatom[WMState], 0L, 2L, False, wmatom[WMState],
-		&real, &format, &n, &extra, (unsigned char **)&p) != Success)
+		&real, &format, &n, &extra, &p) != Success)
 		return -1;
-	if (n != 0)
-		result = *p;
+	if (n != 0 && format == 32)
+		result = *(long *)p;
 	XFree(p);
 	return result;
 }
@@ -1129,45 +1102,6 @@ killclient(const Arg *arg)
 }
 
 void
-loadxrdb()
-{
-  Display *display;
-  char * resm;
-  XrmDatabase xrdb;
-  char *type;
-  XrmValue value;
-
-  display = XOpenDisplay(NULL);
-
-  if (display != NULL) {
-    resm = XResourceManagerString(display);
-
-    if (resm != NULL) {
-      xrdb = XrmGetStringDatabase(resm);
-
-      // if (xrdb != NULL) {
-      //   XRDB_LOAD_COLOR("dwm.normbordercolor", normbordercolor);
-      //   XRDB_LOAD_COLOR("dwm.normbgcolor", normbgcolor);
-      //   XRDB_LOAD_COLOR("dwm.normfgcolor", normfgcolor);
-      //   XRDB_LOAD_COLOR("dwm.selbordercolor", selbordercolor);
-      //   XRDB_LOAD_COLOR("dwm.selbgcolor", selbgcolor);
-      //   XRDB_LOAD_COLOR("dwm.selfgcolor", selfgcolor);
-      // }
-      if (xrdb != NULL) {
-        XRDB_LOAD_COLOR("dwm.color0", normbordercolor);
-        XRDB_LOAD_COLOR("dwm.color8", normbgcolor);
-        XRDB_LOAD_COLOR("dwm.color0", normfgcolor);
-        XRDB_LOAD_COLOR("dwm.color6", selbordercolor);
-        XRDB_LOAD_COLOR("dwm.color0", selbgcolor);
-        XRDB_LOAD_COLOR("dwm.color14", selfgcolor);
-      }
-    }
-  }
-
-  XCloseDisplay(display);
-}
-
-void
 manage(Window w, XWindowAttributes *wa)
 {
 	Client *c, *t = NULL, *term = NULL;
@@ -1183,7 +1117,6 @@ manage(Window w, XWindowAttributes *wa)
 	c->w = c->oldw = wa->width;
 	c->h = c->oldh = wa->height;
 	c->oldbw = wa->border_width;
-	c->cfact = 1.0;
 
 	updatetitle(c);
 	if (XGetTransientForHint(dpy, w, &trans) && (t = wintoclient(trans))) {
@@ -1203,13 +1136,13 @@ manage(Window w, XWindowAttributes *wa)
 	c->y = MAX(c->y, c->mon->wy);
 	c->bw = borderpx;
 
-	selmon->tagset[selmon->seltags] &= ~scratchtag;
-	if (!strcmp(c->name, scratchpadname)) {
-		c->mon->tagset[c->mon->seltags] |= c->tags = scratchtag;
-		c->isfloating = True;
-		c->x = c->mon->wx + (c->mon->ww / 2 - WIDTH(c) / 2);
-		c->y = c->mon->wy + (c->mon->wh / 2 - HEIGHT(c) / 2);
-	}
+ 	selmon->tagset[selmon->seltags] &= ~scratchtag;
+ 	if (!strcmp(c->name, scratchpadname)) {
+ 		c->mon->tagset[c->mon->seltags] |= c->tags = scratchtag;
+ 		c->isfloating = True;
+ 		c->x = c->mon->wx + (c->mon->ww / 2 - WIDTH(c) / 2);
+ 		c->y = c->mon->wy + (c->mon->wh / 2 - HEIGHT(c) / 2);
+ 	}
 
 	wc.border_width = c->bw;
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
@@ -1324,7 +1257,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / refreshrate))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1407,12 +1340,6 @@ propertynotify(XEvent *e)
 	}
 }
 
-void
-quit(const Arg *arg)
-{
-	running = 0;
-}
-
 Monitor *
 recttomon(int x, int y, int w, int h)
 {
@@ -1478,7 +1405,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / refreshrate))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1581,6 +1508,8 @@ sendmon(Client *c, Monitor *m)
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
 	attach(c);
 	attachstack(c);
+	if (c->isfullscreen)
+		resizeclient(c, m->mx, m->my, m->mw, m->mh);
 	focus(NULL);
 	arrange(NULL);
 }
@@ -1622,12 +1551,10 @@ sendevent(Client *c, Atom proto)
 void
 setfocus(Client *c)
 {
-	if (!c->neverfocus) {
+	if (!c->neverfocus)
 		XSetInputFocus(dpy, c->win, RevertToPointerRoot, CurrentTime);
-		XChangeProperty(dpy, root, netatom[NetActiveWindow],
-			XA_WINDOW, 32, PropModeReplace,
-			(unsigned char *) &(c->win), 1);
-	}
+	XChangeProperty(dpy, root, netatom[NetActiveWindow], XA_WINDOW, 32,
+		PropModeReplace, (unsigned char *)&c->win, 1);
 	sendevent(c, wmatom[WMTakeFocus]);
 }
 
@@ -1671,24 +1598,6 @@ setlayout(const Arg *arg)
 		arrange(selmon);
 	else
 		drawbar(selmon);
-}
-
-void
-setcfact(const Arg *arg) {
-	float f;
-	Client *c;
-
-	c = selmon->sel;
-
-	if(!arg || !c || !selmon->lt[selmon->sellt]->arrange)
-		return;
-	f = arg->f + c->cfact;
-	if(arg->f == 0.0)
-		f = 1.0;
-	else if(f < 0.25 || f > 4.0)
-		return;
-	c->cfact = f;
-	arrange(selmon);
 }
 
 /* arg > 1.0 will set mfact absolutely */
@@ -1821,7 +1730,7 @@ spawn(const Arg *arg)
 
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
-	selmon->tagset[selmon->seltags] &= ~scratchtag;
+ 	selmon->tagset[selmon->seltags] &= ~scratchtag;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
@@ -1856,6 +1765,34 @@ tagmon(const Arg *arg)
 }
 
 void
+tile(Monitor *m)
+{
+	unsigned int i, n, h, mw, my, ty;
+	Client *c;
+
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
+		return;
+
+	if (n > m->nmaster)
+		mw = m->nmaster ? m->ww * m->mfact : 0;
+	else
+		mw = m->ww;
+	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
+			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+			if (my + HEIGHT(c) < m->wh)
+				my += HEIGHT(c);
+		} else {
+			h = (m->wh - ty) / (n - i);
+			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+			if (ty + HEIGHT(c) < m->wh)
+				ty += HEIGHT(c);
+		}
+}
+
+void
 togglebar(const Arg *arg)
 {
 	selmon->showbar = !selmon->showbar;
@@ -1879,13 +1816,6 @@ togglefloating(const Arg *arg)
 }
 
 void
-togglefullscr(const Arg *arg)
-{
-  if(selmon->sel)
-    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
-}
-
-void
 togglescratch(const Arg *arg)
 {
 	Client *c;
@@ -1905,6 +1835,13 @@ togglescratch(const Arg *arg)
 		}
 	} else
 		spawn(arg);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
@@ -2448,17 +2385,6 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 }
 
 void
-xrdb(const Arg *arg)
-{
-  loadxrdb();
-  int i;
-  for (i = 0; i < LENGTH(colors); i++)
-                scheme[i] = drw_scm_create(drw, colors[i], 3);
-  focus(NULL);
-  arrange(NULL);
-}
-
-void
 zoom(const Arg *arg)
 {
 	Client *c = selmon->sel;
@@ -2484,8 +2410,6 @@ main(int argc, char *argv[])
 	if (!(xcon = XGetXCBConnection(dpy)))
 		die("dwm: cannot get xcb connection\n");
 	checkotherwm();
-        XrmInitialize();
-        loadxrdb();
 	setup();
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec ps", NULL) == -1)
