9df3d0e9674fe8a8174f4837dfecf51cda13bbda
Pedro Pereira <pedro.hr.pereira@proton.me>
2026-05-17 19:03:17 -0300

    dwmblocks: update scripts names

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,8 +1,8 @@
 static const Block blocks[] = {
-	/*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
-	{ "",       "clock.sh",    60,                    10 },
-    { "",       "volume.sh",   0 ,                    11 },
-    // { "",       "battery.sh",  10,                    12 },
+  /*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
+  { "",       "readclock",   60,                    10 },
+  { "",       "readvolume",  0 ,                    11 },
+  { "",       "readbattery", 10,                    12 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
